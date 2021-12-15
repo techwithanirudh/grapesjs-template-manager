1b890bc634a8c10440f499abb7f0e6e59f131f33
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2021-12-15 20:58:36 +0530

    Update pages.js

--- src/manager/pages.js
+++ src/manager/pages.js
@@ -1,127 +1,139 @@
-import UI from '../utils/ui';
+import UI from "../utils/ui";
 
 export default class PagesApp extends UI {
-    constructor(editor, opts = {}) {
-        super(editor, opts);
-        this.addPage = this.addPage.bind(this);
-        this.selectPage = this.selectPage.bind(this);
-        this.removePage = this.removePage.bind(this);
-        this.isSelected = this.isSelected.bind(this);
-        this.handleNameInput = this.handleNameInput.bind(this);
-        this.openEdit = this.openEdit.bind(this);
+  constructor(editor, opts = {}) {
+    super(editor, opts);
+    this.addPage = this.addPage.bind(this);
+    this.selectPage = this.selectPage.bind(this);
+    this.removePage = this.removePage.bind(this);
+    this.isSelected = this.isSelected.bind(this);
+    this.handleNameInput = this.handleNameInput.bind(this);
+    this.openEdit = this.openEdit.bind(this);
 
-        /* Set initial app state */
-        this.state = {
-            editablePageId: '',
-            isShowing: true,
-            nameText: '',
-            pages: [],
-            loading: false
-        };
-    }
+    /* Set initial app state */
+    this.state = {
+      editablePageId: "",
+      isShowing: true,
+      nameText: "",
+      pages: [],
+      loading: false,
+    };
+  }
 
-    get editableId() {
-        return this.state.editablePageId;
-    }
+  get editableId() {
+    return this.state.editablePageId;
+  }
 
-    onRender() {
-        const { pm, setState, editor } = this;
-        setState({
-            loading: true
-        });
-        setState({
-            pages: [...pm.getAll()]
-        });
-        editor.on('page', () => {
-            setState({
-                pages: [...pm.getAll()]
-            })
-        });
-        setState({
-            loading: false
-        });
-    }
+  onRender() {
+    const { pm, setState, editor } = this;
+    setState({
+      loading: true,
+    });
+    setState({
+      pages: [...pm.getAll()],
+    });
+    editor.on("page", () => {
+      setState({
+        pages: [...pm.getAll()],
+      });
+    });
+    setState({
+      loading: false,
+    });
+  }
 
-    isSelected(page) {
-        return this.pm.getSelected().id === page.id;
-    }
+  isSelected(page) {
+    return this.pm.getSelected().id === page.id;
+  }
 
-    selectPage(e) {
-        this.pm.select(e.currentTarget.dataset.key);
-        this.update();
-    }
+  selectPage(e) {
+    this.pm.select(e.currentTarget.dataset.key);
+    this.update();
+  }
 
-    removePage(e) {
-        this.pm.remove(e.currentTarget.dataset.key);
-        this.update();
-    }
+  removePage(e) {
+    this.pm.remove(e.currentTarget.dataset.key);
+    this.update();
+  }
 
-    openEdit(e) {
-        const { editor } = this;
-        this.setStateSilent({
-            editablePageId: e.currentTarget.dataset.key
-        });
-        editor.Modal.close();
-        editor.SettingsApp.setTab('page');
-        editor.runCommand('open-settings');
-    }
+  openEdit(e) {
+    const { editor } = this;
+    this.setStateSilent({
+      editablePageId: e.currentTarget.dataset.key,
+    });
+    editor.Modal.close();
+    editor.SettingsApp.setTab("page");
+    editor.runCommand("open-settings");
+  }
 
-    editPage(id, name) {
-        const currentPage = this.pm.get(id);
-        currentPage?.set('name', name);
-        this.update()
-    }
+  editPage(id, name) {
+    const currentPage = this.pm.get(id);
+    currentPage?.set("name", name);
+    this.update();
+  }
 
-    addPage() {
-        const { pm } = this;
-        const { nameText } = this.state
-        if (!nameText) return;
-        pm.add({
-            name: nameText,
-            component: ''
-        });
-        this.update();
-    }
+  addPage() {
+    const { pm } = this;
+    const { nameText } = this.state;
+    if (!nameText) return;
+    pm.add({
+      name: nameText,
+      component: "",
+    });
+    this.update();
+  }
 
-    handleNameInput(e) {
-        this.setStateSilent({
-            nameText: e.target.value.trim()
-        })
-    }
+  handleNameInput(e) {
+    this.setStateSilent({
+      nameText: e.target.value.trim(),
+    });
+  }
 
-    renderPagesList() {
-        const { pages, loading } = this.state;
-        const { opts, isSelected } = this;
+  renderPagesList() {
+    const { pages, loading } = this.state;
+    const { opts, isSelected } = this;
 
-        if (loading) return opts.loader || '<div>Loading pages...</div>';
+    if (loading) return opts.loader || "<div>Loading pages...</div>";
 
-        return pages.map((page, i) => `<div 
+    return pages
+      .map(
+        (page, i) => `<div 
                 data-id="${i}" 
                 data-key="${page.id}"  
-                class="page ${isSelected(page) ? 'selected' : ''}"
+                class="page ${isSelected(page) ? "selected" : ""}"
             >
                 <i class="fa fa-file-o" style="margin:5px;"></i>
-                ${page.get('name') || page.id}
-                ${isSelected(page) ? '' : `<span class="page-close" data-key="${page.id}">&Cross;</span>`}
-                <span class="page-edit" data-key="${page.id}"><i class="fa fa-hand-pointer-o"></i></span>
-            </div>`).join("\n");
-    }
+                ${page.get("name") || page.id}
+                ${
+                  isSelected(page)
+                    ? ""
+                    : `<span class="page-close" data-key="${page.id}">&Cross;</span>`
+                }
+                <span class="page-edit" data-key="${
+                  page.id
+                }"><i class="fa fa-hand-pointer-o"></i></span>
+            </div>`
+      )
+      .join("\n");
+  }
 
-    update() {
-        this.$el?.find('.pages').html(this.renderPagesList());
-        this.$el?.find('.page').on('click', this.selectPage);
-        this.$el?.find('.page-edit').on('click', this.openEdit);
-        this.$el?.find('.page-close').on('click', this.removePage);
-    }
+  update() {
+    this.$el?.find(".pages").html(this.renderPagesList());
+    this.$el?.find(".page").on("click", this.selectPage);
+    this.$el?.find(".page-edit").on("click", this.openEdit);
+    this.$el?.find(".page-close").on("click", this.removePage);
+  }
 
-    render() {
-        const { $, editor } = this;
+  render() {
+    const { $, editor } = this;
 
-        // Do stuff on render
-        this.onRender();
-        this.$el?.remove();
+    // Do stuff on render
+    this.onRender();
+    this.$el?.remove();
 
-        const cont = $(`<div style="display: ${this.state.isShowing ? 'flex' : 'none'};" class="pages-wrp">
+    const cont = $(`<div style="display: ${
+      this.state.isShowing ? "flex" : "none"
+    };" class="pages-wrp">
                 <div class="pages">
                     ${this.renderPagesList()}
                 </div>
@@ -129,32 +141,44 @@ export default class PagesApp extends UI {
                     <input 
                         class="tm-input sm" 
                         type="text" 
-                        placeholder="${editor.I18n.t('grapesjs-project-manager.pages.placeholder')}" 
+                        placeholder="${editor.I18n.t(
+                          "grapesjs-project-manager.pages.placeholder"
+                        )}" 
                     />
                 </div>
                 <div class="add-page">
-                    ${editor.I18n.t('grapesjs-project-manager.pages.new')}
+                    ${editor.I18n.t("grapesjs-project-manager.pages.new")}
                 </div>
             </div>`);
-        cont.find('.add-page').on('click', this.addPage);
-        cont.find('input').on('change', this.handleNameInput);
+    cont.find(".add-page").on("click", this.addPage);
+    cont.find("input").on("change", this.handleNameInput);
 
-        this.$el = cont;
-        return cont;
+    const appendTo = opts.appendTo;
+    // Do nothing if the panel was requested to be another element
+    if (appendTo) {
+      var appendEl = document.querySelector(appendTo);
+      appendEl.appendChild(cont);
+    } else {
+      this.$el = cont;
     }
 
-    get findPanel() {
-        return this.editor.Panels.getPanel('views-container');
-    }
+    return cont;
+  }
 
-    showPanel() {
-        this.state.isShowing = true;
-        this.findPanel?.set('appendContent', this.render()).trigger('change:appendContent');
-        this.update();
-    }
+  get findPanel() {
+    return this.editor.Panels.getPanel("views-container");
+  }
 
-    hidePanel() {
-        this.state.isShowing = false;
-        this.render();
-    }
+  showPanel() {
+    this.state.isShowing = true;
+    this.findPanel
+      ?.set("appendContent", this.render())
+      .trigger("change:appendContent");
+    this.update();
+  }
+
+  hidePanel() {
+    this.state.isShowing = false;
+    this.render();
+  }
 }
